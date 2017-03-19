bd68e96e107c77a140d077ca11e207cea578c937
MR.AD <maditya679@gmail.com>
2017-03-19 15:05:51 +0530

    Demo added

---
+++ app/src/main/java/com/example/mrad/demogethub/Demo.java
@@ -0,0 +1,16 @@
+package com.example.mrad.demogethub;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by MR.AD on 019-Mar-3-19-2017.
+ */
+
+public class Demo {
+
+    public static void L(Context context,String msg)
+    {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+}
